d368e39577effbddb610fc52d464180cc8d68326
Glenda <124149468+kittitach1526@users.noreply.github.com>
2026-03-14 14:12:30 +0700

    Update firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from 'key.json';
+import serviceAccount from '../key.json';
 
 // Initialize Firebase Admin
 if (!getApps().length) {
