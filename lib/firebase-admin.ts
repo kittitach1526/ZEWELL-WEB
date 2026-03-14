ca9fa58f0843f404df7ffc467a0727fd6e2b0e1d
Glenda <124149468+kittitach1526@users.noreply.github.com>
2026-03-14 14:15:49 +0700

    Update firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,14 +1,16 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from '../key.json';
 
-// Initialize Firebase Admin
+// ไม่ต้อง import serviceAccount จากไฟล์แล้ว
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT 
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT) 
+  : {};
+
 if (!getApps().length) {
     initializeApp({
-        credential: cert(serviceAccount as any),
+        credential: cert(serviceAccount),
     });
 }
 
 const db = getFirestore();
-
 export { db };
